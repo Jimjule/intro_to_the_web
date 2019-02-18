37c4710bbc071664683d5c34ff4603f3a0f3339a
Jimjule <roadknightj@gmail.com>
2019-02-18 16:01:46 +0000

    Server has three gets

--- app.rb
+++ app.rb
@@ -1,5 +1,16 @@
 require 'sinatra'
+require 'shotgun'
 
 get '/' do
   "hello!"
+end
+
+get '/secret' do
+  "Volo futurus unus"
+end
+
+get '/cat' do
+    "<div style= 'border: dashed red'>
+      <img src='http://bit.ly/1eze8aE'>
+    </div>"
 end
